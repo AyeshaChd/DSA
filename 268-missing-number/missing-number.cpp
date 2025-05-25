61154e43b6c91ab6222cb7f8d1df25e7d837689d
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2025-05-26 00:52:14 +0500

    Time: 0 ms (100.00%) | Memory: 21.7 MB (69.92%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n= nums.size();
+     
+
+        int XOR1 =0; // for N natural numbers which don't have any missing number
+        int XOR2 = 0; // for array elements which have a missing numbers;
+
+        for (int i = 0; i < n ; i++)
+        {
+            XOR2=XOR2^nums[i];
+            XOR1=XOR1^i;
+        }
+        XOR1=XOR1^n;
+        return XOR1^XOR2;
+    }
+};
