ed4729736321b95bcc8fe92c69a806ec38aba433
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2025-11-13 12:30:58 +0500

    Time: 0 ms (100.00%) | Memory: 9.9 MB (25.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,31 +1,60 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+    //    
+    //  did by myself
+    //      int i=0;
+    //      int j=0;
+    //     sort(s.begin(),s.end());
+    //     sort(t.begin(),t.end());
 
-         int i=0;
-         int j=0;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+    //    if(s.length() != t.length())
+    //    {
+    //     return false;
+    //    };
 
-       if(s.length() != t.length())
+    //    while(i<s.length() && j<t.length())
+    //    {
+    //       if(s[i] !=t[j]  )
+    //       {
+    //         return false;
+    //       }
+    //       i++;
+    //       j++;
+    //    };
+
+    //    return true;
+    // optimized 
+
+       if(s.length()!=t.length())
        {
         return false;
-       };
 
-       while(i<s.length() && j<t.length())
-       {
-          if(s[i] !=t[j]  )
-          {
+       }
+    vector<int>freq(26,0);
+
+    for(int i=0;i<s.length();i++)
+    {
+        freq[s[i]-'a']++;  //  a=97 , 97-97=0,98-97=1;
+
+        freq[t[i]-'a']--;
+    }
+
+    for(int i=0; i<freq.size();i++)
+    {
+        if(freq[i]!=0)
+        {
             return false;
-          }
-          i++;
-          j++;
-       };
+        }
+    }
+    return true;
+    };
 
-       return true;
+
+
 
        
         
         
-    };
+    
 };
