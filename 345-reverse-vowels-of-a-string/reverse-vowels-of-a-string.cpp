2de36ca3bcc2dfe3826a52537e65c186360f3d91
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2025-11-13 15:12:19 +0500

    Time: 0 ms (100.00%) | Memory: 10.2 MB (93.76%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -12,50 +12,71 @@ public:
             else return  false;
         }
     string reverseVowels(string s) {
-        int left=0;
-        int n=s.length();
-        int right=n-1;
-        int count=0;
-        char temp;
+        // int left=0;
+        // int n=s.length();
+        // int right=n-1;
+        // int count=0;
+        // char temp;
 
        
 
-        for(int i=0; i<s.length();i++)
-        {
+        // for(int i=0; i<s.length();i++)
+        // {
 
-            if(left < right)
-        {
-            if (!(isVowel(s[left])) )
-          {
-            left++;
+        //     if(left < right)
+        // {
+        //     if (!(isVowel(s[left])) )
+        //   {
+        //     left++;
           
-            count++;
+        //     count++;
             
-          };
+        //   };
         
-            if (!(isVowel(s[right])))
-          {
-            right--;
-            count++;
-          };
+        //     if (!(isVowel(s[right])))
+        //   {
+        //     right--;
+        //     count++;
+        //   };
 
          
-         if( count == 0)
-         {
-            temp=s[left];
-            s[left]= s[right];
-            s[right]= temp;
+        //  if( count == 0)
+        //  {
+        //     temp=s[left];
+        //     s[left]= s[right];
+        //     s[right]= temp;
             
-            right--;
-            left++;
+        //     right--;
+        //     left++;
 
-         }
+        //  }
 
-         count=0;
+        //  count=0;
 
 
-        };
+        // };
+        // };
+        // return s;
+        int i=0;
+        int j=s.length()-1;
+        while(i<j)
+        {
+            if(!(isVowel(s[i])))
+            {
+                i++;
+            };
+            if(!(isVowel(s[j])))
+            {
+                j--;
+            };
+            if( isVowel(s[i]) && isVowel(s[j]) )
+            {
+                swap( s[i], s[j]);
+                i++;
+                j--;
+            }
         };
         return s;
+
     };
 };
