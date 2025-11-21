cbf48ddf0c942ac1db4266f59442c35c36e097ec
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2025-11-21 16:43:46 +0500

    Time: 11 ms (6.16%) | Memory: 12.4 MB (85.12%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int count = 0;
+        vector<int> ans;
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < nums2.size(); j++) {
+                if (nums1[i] == nums2[j]) {
+                    count++;
+                }
+                if (count !=0) {
+                    if (nums1[i] < nums2[j]) {
+                        ans.push_back(nums2[j]);
+                        break;
+                    }
+                };
+                if (j == nums2.size() - 1) {
+                    ans.push_back(-1);
+                };
+                
+            };
+            count=0;
+        };
+        return ans;
+    };
+};
