9a1cb98e875fa818d52d34182fc72e4498a07d2a
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2025-12-12 15:07:12 +0500

    Time: 1 ms (76.62%) | Memory: 15.8 MB (8.96%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-        int x = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]] = i;
-        };
-        for (int i = 0; i < nums.size(); i++) {
-            x = target - nums[i];
-            if (mp.find(x) != mp.end()) {
-                if (mp[x] != i)
-                    return {mp[x], i};
-            };
-        };
-        return {};
-    };
+    //     unordered_map<int, int> mp;
+    //     int x = 0;
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         mp[nums[i]] = i;
+    //     };
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         x = target - nums[i];
+    //         if (mp.find(x) != mp.end()) {
+    //             if (mp[x] != i)
+    //                 return {mp[x], i};
+    //         };
+    //     };
+    //     return {};
+    // };
+    // we can also do finding from  and storing in hashmap at same time;
+    unordered_map<int,int> m;
+        int one = 0, two = 0;
+        for(int i=0;i<nums.size();i++){
+            
+            int rem = target-nums[i];
+            if(m.find(rem)!=m.end()){
+                one = m[rem];
+                two = i;
+            }else{
+                m[nums[i]]=i;
+            }
+
+        }
+        return {one,two};
+    }
 };
