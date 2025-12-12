ded0218579e41d4ee67292edd5f7e31fa9f99cef
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2025-12-12 17:21:48 +0500

    Time: 3 ms (98.11%) | Memory: 37.6 MB (26.27%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -2,37 +2,25 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
        
-        unordered_map<int, int> mp1;
-        unordered_map<int, int> mp2;
-        vector<int>v1;
-        vector<int>v2;
-        unordered_set<int>s1(nums1.begin(),nums1.end());
-        unordered_set<int>s2(nums2.begin(),nums2.end());
+        // 
+        
+        // -----------------------------doing only using set without hasmap
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
+        
+        vector<int> distinct_nums1, distinct_nums2;
+        for (int num : set1) {
+            if (set2.count(num) == 0) {
+                distinct_nums1.push_back(num);
+            }
+        }
 
-        // we are using this type of loop bcz in sets elements can't acccessed by indexing.
-        for (int x : s1) {
+        for (int num : set2) {
+            if (set1.count(num) == 0) {
+                distinct_nums2.push_back(num);
+            }
+        }
 
-            mp1[x]++;
-        };
-
-        for (int y : s2) {
-            mp2[y]++;
-        };
-        for (int x: s1) {
-            if (mp2.find(x) ==  mp2.end() ) {
-                
-                
-                v1.push_back(x);
-                
-               
-            };
-        };
-            for (int x: s2) {
-                if (mp1.find(x) == mp1.end() ) {
-                  v2.push_back(x);
-                };
-            };
-               
-                return {v1,v2};
+        return {distinct_nums1, distinct_nums2};
             };
         };
