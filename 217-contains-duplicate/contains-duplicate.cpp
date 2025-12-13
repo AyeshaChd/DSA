0aa5b8ec23fb466e6ab552ce5fc310f30e168596
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2025-12-13 13:41:19 +0500

    Time: 122 ms (5.68%) | Memory: 95 MB (6.06%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        map<int,int>mp;
+        for(int i=0; i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        };
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(mp.find(nums[i]))
+        // }
+         for(auto it :mp)
+         {
+            if(it.second!=1)
+            {
+                return true;
+            }
+         };
+         return false;
+    }
+};
