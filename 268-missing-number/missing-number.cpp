49f26ccae72d771a05f9c4b296f61a6189de8c6c
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2025-12-13 12:46:57 +0500

    Time: 0 ms (100.00%) | Memory: 21.8 MB (70.09%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // int n= nums.size();
+        // ------------------------by myself------------------
+        //  map<int, int> mp;
+        //         int n = nums.size();
+        //         int missing_number =0;
+        //         for (int i = 0; i < nums.size(); i++) {
+        //             mp[nums[i]] = nums[i];
+        //         };
+        //         for (int i = 0; i < n; i++) {
+        //             if (mp.find(i + 1) == mp.end())
+        //              { missing_number = i + 1; };
+        //         };
+        //         return missing_number;
+        // -----------------------------------------------
+        // -----------------------------------------------
 
-        // int XOR1 =0; // for N natural numbers which don't have any missing
-        // number int XOR2 = 0; // for array elements which have a missing
-        // numbers;
+        // XOR sign ^.5 ^ 5 = 0 number with it self gives zero 5 ^ 0 =5 number with zero gives number it self.
+        
+        int n =nums.size();
 
-        // for (int i = 0; i < n ; i++)
-        // {
-        //     XOR2=XOR2^nums[i];
-        //     XOR1=XOR1^i;
-        // }
-        // // considering first ex.
-        // //ZOR1 = 1^2 after loop end
-        //  // as i<n but we have to include n as well
-        // XOR1=XOR1^n;  //  1^2^3
-        // return XOR1^XOR2;
+        int XOR1 = 0; // for N natural numbers which don't have any missing number 
+        int XOR2 = 0; // for array elements which have a missing numbers;
 
-        map<int, int> mp;
-        int n = nums.size();
-        int missing_number =0;
-        for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]] = nums[i];
-        };
         for (int i = 0; i < n; i++) {
-            if (mp.find(i + 1) == mp.end())
-             { missing_number = i + 1; };
-        };
-        return missing_number;
+            XOR2 = XOR2 ^ nums[i];
+            XOR1 = XOR1 ^ i;
+        }
+        // considering first ex.
+        // ZOR1 = 1^2 after loop end
+        // as i<n but we have to include n as well
+        XOR1 = XOR1 ^ n; //  1^2^3    and   xor2 is 3^1
+        return XOR1 ^ XOR2;  
+        // -- -- -All common numbers cancel out, Remaining value = missing number
     };
 };
