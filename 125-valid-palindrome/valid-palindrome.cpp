ef4adb114b30580612d53f9aa07c770837e710cc
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2025-12-14 16:26:30 +0500

    Time: 0 ms (100.00%) | Memory: 10 MB (56.79%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,28 +2,53 @@ class Solution {
 public:
    
     bool isPalindrome(string s) {
-        vector<char> t;
+
+        // myapproach
+//         vector<char> t;
        
 
-        for (int i = 0; i < s.length(); i++) {
-            if (isalnum(s[i])) { t.push_back(tolower(s[i]));
-            };
+//         for (int i = 0; i < s.length(); i++) {
+//             if (isalnum(s[i])) { t.push_back(tolower(s[i]));
+//             };
            
-        };
-         int left = 0;
-        int right = t.size() - 1;
+//         };
+//          int left = 0;
+//         int right = t.size() - 1;
     
-        while (left<right) {
-            if (t[left] != t[right])
-            {
-  return false;
-            }
+//         while (left<right) {
+//             if (t[left] != t[right])
+//             {
+//   return false;
+//             }
                 
           
-            left++;
-            right--;
-        };
+//             left++;
+//             right--;
+//         };
 
-        return true;
-    }
+//         return true;
+// ----------optimized---------
+
+
+
+
+
+
+int right=s.size()-1;
+int left=0;
+while(left<right)
+{
+    if(!isalnum(s[left])) // means keep iterating until we find alphanumeric numeric number
+    { left++;
+    continue;
+    };
+    if(!isalnum(s[right])) {right--;
+    continue;};
+
+    if(tolower(s[left])!=tolower(s[right])) return false;
+    left++;
+    right--;
+};
+return true;
+    };
 };
