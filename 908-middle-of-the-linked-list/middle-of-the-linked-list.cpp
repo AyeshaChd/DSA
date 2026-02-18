6c0fc320762f0b19dc9a7021e34b9688d7569b43
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2026-02-18 18:08:52 +0500

    Time: 0 ms (100.00%) | Memory: 10 MB (63.08%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -11,41 +11,68 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-      
-        int totalLength = 0;
-        int count = 0;
-                if (head == NULL || head->next == NULL)
-            return head;
-        while (temp != NULL) {
-              totalLength ++;
-              temp=temp->next;
-        };
-       
-        temp=head;
-        while (temp != NULL) {
-          
-            count++;
-            
-            if (totalLength % 2 == 0) {
-             
-                if (count == (totalLength / 2) + 1) {
-                   
-                   cout<<temp->val;
-                    return temp;
 
-                };
-            };
-            if (totalLength % 2 != 0) {
+        // --------------------my approach
+        // ListNode* temp = head;
+      
+        // int totalLength = 0;
+        // int count = 0;
+        //         if (head == NULL || head->next == NULL)
+        //     return head;
+        // while (temp != NULL) {
+        //       totalLength ++;
+        //       temp=temp->next;
+        // };
+       
+        // temp=head;
+        // while (temp != NULL) {
+          
+        //     count++;
+            
+        //     if (totalLength % 2 == 0) {
+             
+        //         if (count == (totalLength / 2) + 1) {
+                   
+        //            cout<<temp->val;
+        //             return temp;
+
+        //         };
+        //     };
+        //     if (totalLength % 2 != 0) {
               
-                if (count == (totalLength + 1) / 2) {
-                    return temp;
+        //         if (count == (totalLength + 1) / 2) {
+        //             return temp;
                   
-                };
+        //         };
                
-            };
-             temp=temp->next;
-        };
+        //     };
+        //      temp=temp->next;
+        // };
+        // return head;
+
+        // same concept as mine but only improved version
+
+    if (head == NULL || head->next == NULL)
         return head;
+
+    ListNode* temp = head;
+    int totalLength = 0;
+
+    // Count length
+    while (temp != NULL) {
+        totalLength++;
+        temp = temp->next;
+    }
+
+    int middle = (totalLength / 2) + 1; // it gives middle of both even or odd;
+    temp = head;
+
+    for (int i = 1; i < middle; i++) {
+        temp = temp->next;
+    }
+
+    return temp;
+
+
     };
 };
