bc029449a7091456863c5b895f73ee4d3681ffa2
Ayesha Kanwal <161125297+AyeshaChd@users.noreply.github.com>
2026-07-06 01:21:35 +0500

    Time: 3 ms (51.41%), Space: 30.3 MB (40.05%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-
-        for(int i=0; i<nums.size();i++){
-            nums[i]=nums[i]* nums[i];
-
-        };
-       sort(nums.begin(),nums.end());
-        return nums;
+        int n=nums.size();
+        vector<int>res(n,0);
+        int left=0; int right=n-1;
+        int i=n-1;
+        // as array  is already sorted then means largest elements will be at end and start, we will compare them and put large value in new array  and move index
+       while(left<right)
+       {
+         if(nums[left] * nums[left] >  nums[right] * nums[right]|| nums[left] * nums[left] == nums[right] * nums[right])
+         {
+            res[i]=nums[left] * nums[left];
+         left ++;
+         i --;}
+         else {
+            res[i]=nums[right] * nums[right];
+            right--;
+            i--;
+         }
+       }
+       // as when loop ends last smaller value is not pushed in res
+       res[i]= nums[left]*nums[left];
+       return res;
+       
     }
 };
