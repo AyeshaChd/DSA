eb01f86b02d66e96193cdcbcc73865c478877c3f
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-07 11:16:47 +0500

    Time: 611 ms (7.13%), Space: 9.8 MB (49.71%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>lowerCaseArray(26,0);
+        int i = 0;
+        int j = 0;
+
+        while (i < s.size() || i < t.size()) {
+            if ( i < s.size()) {
+                lowerCaseArray[s[i] - 'a'] = lowerCaseArray[s[i] - 'a'] + 1;
+                cout<<"s:"<<lowerCaseArray[s[i] - 'a'] <<endl;
+            }
+            if (i < t.size()) {
+                lowerCaseArray[t[i] - 'a'] = lowerCaseArray[t[i] - 'a'] - 1;
+                  cout<<"t:"<<lowerCaseArray[t[i] - 'a'] <<endl;
+            }
+            i++;
+        };
+        while (j < lowerCaseArray.size()) {
+            if (lowerCaseArray[j] != 0){
+            cout<<j<<"j:"<<lowerCaseArray[j]<<endl;
+                return false;}
+                j++;
+        };
+        return true;
+    };
+};
