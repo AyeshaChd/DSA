3967f66417a49beeee11a82685e8270956fe1fd5
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-07 14:18:42 +0500

    Time: 0 ms (100%), Space: 10.6 MB (8.63%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,35 +1,52 @@
 class Solution {
 public:
     bool isAlNum(char c) {
-          cout<<"bookkk"<<endl;
+      
         if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
             (c >= '0' && c <= '9'))
-          
+
             return true;
-        else return false;
+        else
+            return false;
     }
 
     bool isPalindrome(string s) {
         if (s == " ")
             return true;
+       
+        vector<char> finals;
+         int i = 0;
+       
         int left = 0;
         int right = s.size() - 1;
-        while (left < right) {
-            // inner while loops so left and right not cross each other
-            while (left < right && !isAlNum(s[left]))
-                left++;
-            while (left < right && !isAlNum(s[right]))
-                right--;
+        while (i < s.size() ) {
+            if (isAlNum(s[i]))
+                {finals.push_back(tolower(s[i])); }
+             i++;
+        }
+         int k=0;;
+         int j = finals.size() - 1;
+        while (k < j) {
+            if(finals[k] != finals[j]) return false;
+            k++;
+            j--;
+        }
+        ///// optimized if not want to use  space
+        // while (left < right) {
+        //     // inner while loops so left and right not cross each other
+        //     while (left < right && !isAlNum(s[left]))
+        //         left++;
+        //     while (left < right && !isAlNum(s[right]))
+        //         right--;
 
-            if (tolower(s[left]) != tolower(s[right])) {
-                return false;
-            };
+        //     if (tolower(s[left]) != tolower(s[right])) {
+        //         return false;
+        //     };
 
-            left++;
-            right--;
-        };
-    
-    return true;
+        //     left++;
+        //     right--;
+        // };
+
+        return true;
+    };
 };
-}
-;
