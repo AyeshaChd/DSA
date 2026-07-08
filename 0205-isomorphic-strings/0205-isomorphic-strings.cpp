9ed7c4dedf5e8624d57ae3d1a8710129caefc831
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-09 01:22:19 +0500

    Time: 5 ms (11.05%), Space: 9.3 MB (82.14%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,24 +2,38 @@
     class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int> indexS(200, 0); // Stores index of characters in string s
-        vector<int> indexT(200, 0); // Stores index of characters in string t
+        // vector<int> indexS(200, 0); // Stores index of characters in string s
+        // vector<int> indexT(200, 0); // Stores index of characters in string t
         
-        int len = s.length(); // Get the length of both strings
+        // int len = s.length(); // Get the length of both strings
         
-        if(len != t.length()) { // If the lengths of the two strings are different, they can't be isomorphic
-            return false;
-        }
+        // if(len != t.length()) { // If the lengths of the two strings are different, they can't be isomorphic
+        //     return false;
+        // }
         
-        for(int i = 0; i < len; i++) { // Iterate through each character of the strings
-            if(indexS[s[i]] != indexT[t[i]]) { // Check if the index of the current character in string s is different from the index of the corresponding character in string t
-                return false; // If different, strings are not isomorphic
-            }
+        // for(int i = 0; i < len; i++) { // Iterate through each character of the strings
+        //     if(indexS[s[i]] != indexT[t[i]]) { // Check if the index of the current character in string s is different from the index of the corresponding character in string t
+        //         return false; // If different, strings are not isomorphic
+        //     }
             
-            indexS[s[i]] = i + 1; // updating position of current character
-            indexT[t[i]] = i + 1;
-        };
+            // indexS[s[i]] = i + 1; // updating position of current character
+        //     // indexT[t[i]] = i + 1;
+        // };
         
-        return true; // If the loop completes without returning false, strings are isomorphic
+        // return true; // If the loop completes without returning false, strings are isomorphic
+
+        map<char,char>room;
+        map<char,char>room2;
+        if(s.length() != t.length()) return false;
+        for(int i=0; i<s.size();i++)
+        {
+            if(room.find(s[i])!= room.end() && room[s[i]] != t[i] || room2.find(t[i])!= room2.end() && room2[t[i]] != s[i]){
+                return false;
+            };
+            room[s[i]]=t[i];
+            room2[t[i]]=s[i];
+
+        };
+        return true;
     };
 };
