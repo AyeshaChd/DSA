ac8ff03390d1accadc3721fd84eda79a5f04eedc
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-12 00:54:44 +0500

    Time: 3 ms (11.5%), Space: 8.7 MB (25.55%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int count = 0;
+        unordered_map<char,char>hashmap;
         for (int i = 0; i < jewels.size(); i++) {
-            for (int j = 0; j < stones.size(); j++) {
-                if (jewels[i] == stones[j])
-                    count++;
-            }
+            hashmap[jewels[i]];
+            };
+        for(int i=0; i<stones.size();i++)
+        {
+            if(hashmap.find(stones[i])!=hashmap.end())count++;
         };
+
+        
         return count;
     };
 };
