719e69d02fd4a629a0fab73023ec3a891ba0d8b0
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-12 01:37:03 +0500

    Time: 5 ms (26.32%), Space: 9.8 MB (25.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int>lowerCaseArray(26,0);
-        int i = 0;
-        int j = 0;
+        // vector<int>lowerCaseArray(26,0);
+        // int i = 0;
+        // int j = 0;
 
-        while (i < s.size() || i < t.size()) {
-            if ( i < s.size()) {
-                lowerCaseArray[s[i] - 'a'] = lowerCaseArray[s[i] - 'a'] + 1;
-                cout<<"s:"<<lowerCaseArray[s[i] - 'a'] <<endl;
-            }
-            if (i < t.size()) {
-                lowerCaseArray[t[i] - 'a'] = lowerCaseArray[t[i] - 'a'] - 1;
-                  cout<<"t:"<<lowerCaseArray[t[i] - 'a'] <<endl;
-            }
-            i++;
-        };
-        while (j < lowerCaseArray.size()) {
-            if (lowerCaseArray[j] != 0){
-            cout<<j<<"j:"<<lowerCaseArray[j]<<endl;
-                return false;}
-                j++;
+        // while (i < s.size() || i < t.size()) {
+        //     if ( i < s.size()) {
+        //         lowerCaseArray[s[i] - 'a'] = lowerCaseArray[s[i] - 'a'] + 1;
+        //         cout<<"s:"<<lowerCaseArray[s[i] - 'a'] <<endl;
+        //     }
+        //     if (i < t.size()) {
+        //         lowerCaseArray[t[i] - 'a'] = lowerCaseArray[t[i] - 'a'] - 1;
+        //           cout<<"t:"<<lowerCaseArray[t[i] - 'a'] <<endl;
+        //     }
+        //     i++;
+        // };
+        // while (j < lowerCaseArray.size()) {
+        //     if (lowerCaseArray[j] != 0){
+        //     cout<<j<<"j:"<<lowerCaseArray[j]<<endl;
+        //         return false;}
+        //         j++;
+        // };
+        // return true;
+        if(s.size()!= t.size()) return false;
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]]=mp[s[i]]+1;
+            mp[t[i]]=mp[t[i]]-1; 
+        }
+        for(auto it:mp)
+        {
+            if(it.second != 0) return false;
         };
         return true;
     };
