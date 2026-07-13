22c736979150e81b4c36a7e80558c8bc409f45d0
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-14 00:05:22 +0500

    Time: 0 ms (100%), Space: 23.9 MB (56.17%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // int j= -1;
-      
-        // for(int i=0; i<nums.size();i++)
-        // {
-        //     if(nums[i] == 0 && j== -1) 
-        //      { j = i;}
-            
-        //      if(nums[i] != 0 && j != -1) 
-        //      { nums[j] = nums[i];
-        //        nums[i]=0;
-        //        j++;
-        //      };
-         //}    
-             int j=0;
-             for(int i=0;
-             i<nums.size();i++){
-                if(nums[i]!= 0)
-                { nums[j]= nums[i];
-                j++;}
-               
-             };
-             for(int i=j ;
-             i<nums.size();i++){
-                nums[i]= 0;
-               
-             };
-
-
-    
+        int j=0;
+        for(int i=0; i<nums.size();i++)
+        {
+            if(nums[i] != 0)
+            { 
+                nums[j]=nums[i];
+                j++;
+            };
+        };
+        while(j<nums.size())
+        {
+            nums[j]= 0;
+            j++;
+        };
       
     };
 };
