7f5d3297354e596b354b589dd96f59115d8f8778
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-15 09:05:09 +0500

    Time: 67 ms (11.53%), Space: 34.2 MB (11.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,11 +4,20 @@ public:
         unordered_map<string,vector<string>>mp;
         vector<vector<string>>ans;
         for(int i=0; i<strs.size();i++)
-        {
+        {      int count[26]={0};
+       
                     string original= strs[i];
                     string s=strs[i];
-                 sort(s.begin(),s.end());
-                    mp[s].push_back(original);
+                    for(char c : s)
+                    {
+                       count[c - 'a']++;
+                    }
+              string key="";
+              for(int i=0; i<26;i++)
+              {
+key+=to_string(count[i])+ "#";
+              }
+                    mp[key].push_back(original);
         };
         for(auto it : mp)
         {
@@ -18,3 +27,4 @@ public:
         return ans;
     };
 };;
+// //  sort(s.begin(),s.end()); if sort is allow put sorted string as key and push original;
