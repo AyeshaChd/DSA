3a6baeeeb3487abd9c43a42e5235f7aed4778017
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-16 22:30:53 +0500

    Time: 100 ms (31.52%), Space: 69.8 MB (63.05%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+      
+        if(s.size()==1 || s.size()== 2) return s;
+        int m=s.size()/2;
+        string half=s.substr(0,m);//half string generation
+        cout<<"half:"<<half;
+        sort(half.begin(),half.end());
+        cout<<"halfsort:"<<half; 
+        string rev=half; // reversal
+        reverse(rev.begin(),rev.end());
+         cout<<"rev:"<<rev; 
+         cout<<"m:"<<m;
+        if(s.size()%2==1) half+=s[m]; // append middle char as is if n is odd
+        return half + rev;   // concatenate
+        
+    }
+};
