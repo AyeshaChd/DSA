a0f4bc4b1fdfd07267f5deeb804aac4a9962a6a4
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-17 16:27:57 +0500

    Time: 0 ms (100%), Space: 8.3 MB (97.38%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string makeGood(string s) {
+      
+        string res="";
+
+        for(int i=0 ; i<s.length();i++)
+        {
+            if(!res.empty() && abs(res.back()-s[i])==32)
+            {
+                res.pop_back();
+            }
+            else res+=s[i];
+  
+};return res;
+    };
+};
