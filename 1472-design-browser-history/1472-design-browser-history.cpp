d509293629062ceffa46993a638747cc7770af06
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-26 00:35:34 +0500

    Time: 15 ms (90.03%), Space: 62.6 MB (87.46%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -1,27 +1,44 @@
 class BrowserHistory {
-    vector<string>s;
- int i=NULL;
+    vector<string> s;
+    int i = NULL;
+
 public:
     BrowserHistory(string homepage) {
         s.push_back(homepage);
-           i=0;
+        i = 0;
     }
-    
+
     void visit(string url) {
-        s.resize(i+1);
-         s.push_back(url);
-         i=s.size()-1;
+        s.resize(i + 1); // in browzer history means where i was lasttime
+                         // directly comes to last pushed element and those
+                         // indexes who were never forwarded totaly deleted
+                       //history = ["A", "B", "C", "D"]
+                         // i = 1   // Currently at "B"
+
+                          // You want to keep:
+
+                           // ["A", "B"]
+
+                        // which has 2 elements.
+
+                        // Using resize(i + 1) ✅
+        s.push_back(url);
+        i = s.size() - 1;
     }
-    
+
     string back(int steps) {
-      if(i-steps<0) i=0;
-      else i=i-steps;
+        if (i - steps < 0)
+            i = 0;
+        else
+            i = i - steps;
         return s[i];
     }
-    
+
     string forward(int steps) {
-       if(i+steps>=s.size()) i=s.size()-1;
-       else i= i+steps;
+        if (i + steps >= s.size())
+            i = s.size() - 1;
+        else
+            i = i + steps;
 
         return s[i];
     }
