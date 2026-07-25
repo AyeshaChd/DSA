785c61de23d061fdcdb906aa2796303f287e0f39
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-26 01:36:34 +0500

    Time: 181 ms (6.21%), Space: 45.9 MB (95.07%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,22 +1,38 @@
 class MyHashSet {
 
-    unordered_set<int>v;
+    // unordered_set<int>v;
+    vector<int>v;
 public:
     MyHashSet() {
         
     }
     
     void add(int key) {
-        v.insert(key);
+        // v.insert(key);
+        for(int i=0;i<v.size();i++)
+    {
+        if(v[i]==key) return;
+    }
+    
+        v.push_back(key);
     }
     
     void remove(int key) {
-       if(v.find(key) != v.end()) v.erase(key);
+    //    if(v.find(key) != v.end()) v.erase(key);
+    for(int i=0;i<v.size();i++)
+    {
+        if(v[i]==key) v[i]=-1;
+    }
     }
     
     bool contains(int key) {
-       if(v.find(key) != v.end()) return true;
-       else return false;
+    //    if(v.find(key) != v.end()) return true;
+    //    else return false;
+    for(int i=0;i<v.size();i++)
+    {
+        if(v[i]==key) return true;
+    }
+    return false;
     }
 };
 
