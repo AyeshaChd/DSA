e82ddf05a8e89aa084e50249c2741621e0fa5e10
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-28 17:29:08 +0500

    Time: 0 ms (100%), Space: 11.2 MB (35.48%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -3,15 +3,32 @@ public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
          int target=tickets[k];
          int total_time=0;
+      queue<int>q;
          for(int i=0; i<tickets.size();i++)
          {
-            // if(i<=k)
-            // {
-total_time+=min(target,tickets[i]);
-            // }
-            // else{
-            //     total_time+=min(target-1,tickets[i]);
-            // }
+             q.push(i);
+         }
+         while( !q.empty())
+         {
+            if(tickets[q.front()]-1 != 0)
+            {
+              tickets[q.front()]-= 1; 
+              q.push(q.front());
+              q.pop();
+              total_time++;
+            }
+            if( q.front()== k && tickets[q.front()]-1 == 0)
+            {
+total_time++;
+break;
+            }
+            if(  tickets[q.front()]-1 == 0)
+            {
+                tickets[q.front()]=0;
+                total_time++;
+                q.pop();
+            }
+            
          }
          return total_time; 
     }
