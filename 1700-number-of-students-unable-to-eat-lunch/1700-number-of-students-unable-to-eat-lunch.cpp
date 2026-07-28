871c5591e7c1a0b0c1a1ccf62ba85baa2e9d926f
Ayesha Chaudhary <161125297+AyeshaChd@users.noreply.github.com>
2026-07-28 15:00:07 +0500

    Time: 0 ms (100%), Space: 11.9 MB (39.62%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-         int arr[2]={0,0};
-        for(int i=0; i< students.size();i++)
-        {
-            arr[students[i]]++;
-        };
-        for(int i:sandwiches)
-        {
-            if(arr[i]==0)
-            {
-                break;
-            }
-            arr[i]--;
-        }
-       return arr[0]+arr[1];
-       
-        
+    int i=0;
+    int count =0;  // Number of students rotated without taking sandwich
+    queue<int>q;
+    for(int s :students)
+    {
+    q.push(s);
     }
+   while(!q.empty())
+   {
+    if(sandwiches[i]== q.front())
+    {
+        q.pop();
+        i++;
+        count=0;
+    }
+    else{
+     q.push(q.front());
+     q.pop();
+     count++;
+     if(count==q.size())
+     break;
+    }
+    
+   }
+    return q.size();
+    };
 };
